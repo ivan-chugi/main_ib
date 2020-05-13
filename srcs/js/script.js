4b2e8a065fe4ae09fc9a154dd94189877e83ddca
ivan-chugi <wcspoiler@yandex.ru>
2020-05-13 21:24:12 +0300

    Mobile version

--- srcs/js/script.js
+++ srcs/js/script.js
@@ -1,18 +1,10 @@
-const button_about = document.querySelector("#about");
-const button_projects = document.querySelector("#projects");
-const slide_about = document.querySelector(".slide-about");
-const slide_projects = document.querySelector(".slide-projects");
-
-button_projects.addEventListener("change", function() {
-    if (button_projects.checked) {
-        slide_about.style.transform = "translateX(-100%)";
-        slide_projects.style.transform = "translateX(-100%)";
-    }
-});
-
-button_about.addEventListener("change", function() {
-    if (button_about.checked) {
-        slide_about.style.transform = "translateX(-100%)";
-        slide_projects.style.transform = "translateX(100%)";
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    let timer;
+    window.addEventListener("resize", function() {
+        document.body.classList.remove("animated");
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+        document.body.classList.add("animated"); },
+        100);
+    });
 });
